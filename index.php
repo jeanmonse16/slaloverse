6ed8908b134e2f8d9111a642dc0fc474b1e5705d
jeanpier <jeanpier.dev@gmail.com>
2019-12-05 15:07:15 -0400

    home

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="global.css">
+    <title>Document</title>
+</head>
+<body>
+  <div class="header">
+    <img class="header__img" src="./images/slaloverse.png"/>
+  </div>
+  <div class="navbar">
+    <a>Inicio<a>
+    <a>Tickets<a>
+    <a>Slalo Awards<a>
+    <a>Sign In<a>
+    <a>Sign Up<a>
+  </div>
+  <img src="./images/download.jpg" class="home"/>
+</body>
+</html>
