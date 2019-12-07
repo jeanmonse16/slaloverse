56262d54b5b785089de44703cf0034d37bf3ee2c
jeanpier <jeanpier.dev@gmail.com>
2019-12-07 01:25:03 -0400

    pedazo de estilosssss

--- view/index.php
+++ view/index.php
@@ -4,22 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv=”Expires” content=”0″>
     <link rel="stylesheet" href="../styles/global.css">
-    <title>Document</title>
+    <title>Bienvenido al Slaloverse</title>
 </head>
 <body>
-  <div class="header">
-    <img class="header__img" src="../images/slaloverse.png"/>
-  </div>
   <div class="navbar">
-    <a>Inicio<a>
-    <a>Tickets<a>
-    <a>Slalo Awards<a>
-    <a href="./login.php">Sign In<a>
-    <a>Sign Up<a>
+    <div class="navbar__row1">
+      <a><img src="../images/Slaloverse2.png" /><a>
+      <a>Tickets<a>
+      <a>Slalo Awards<a>
+    </div>
+    <div class="navbar__row2">
+      <a href="./login">Sign In<a>
+      <a>Sign Up<a>
+    </div>
   </div>
-  <div class="home">
-    <img src="../images/Portada_1.jpg"/>
-  </div>
+  <section class="carousel">
+    <div class="carousel__container">
+      <div class="carousel__item">
+        <img class="carousel-item__img" src="../images/Portada_1.jpg">
+        <div class="carousel-item__details">
+          <p class="carousel-item__details--title"> Welcome </p>
+        </div>
+      </div>
+      <div class="carousel__item">
+        <img class="carousel-item__img" src="../images/fest1.jpg">
+        <div class="carousel-item__details">
+          <p class="carousel-item__details--title"> Welcome </p>
+        </div>
+      </div>
+      <div class="carousel__item">
+        <img class="carousel-item__img" src="../images/fest2.jpg">
+        <div class="carousel-item__details">
+          <p class="carousel-item__details--title"> Welcome </p>
+        </div>
+      </div>
+      <div class="carousel__item">
+        <img class="carousel-item__img" src="../images/fest3.jpg">
+        <div class="carousel-item__details">
+          <p class="carousel-item__details--title"> Welcome </p>
+        </div>
+      </div>
+      <div class="carousel__item">
+        <img class="carousel-item__img" src="../images/fest4.jpg">
+        <div class="carousel-item__details">
+          <p class="carousel-item__details--title"> Welcome </p>
+        </div>
+      </div>
+      <div class="carousel__item">
+        <img class="carousel-item__img" src="../images/fest5.jpg">
+        <div class="carousel-item__details">
+          <p class="carousel-item__details--title"> Welcome </p>
+        </div>
+      </div>
+    </div>
+  </section>  
 </body>
 </html>
