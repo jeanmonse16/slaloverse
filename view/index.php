665e0a0d61b4079db66afecce3edab63c4037ce2
jeanpier <jeanpier.dev@gmail.com>
2019-12-07 20:44:13 -0400

    terminando el home

--- view/index.php
+++ view/index.php
@@ -21,6 +21,7 @@
     </div>
   </div>
   <section class="carousel">
+    <h1> Únete al Slaloverse !</h1>
     <div class="carousel__container">
       <div class="carousel__item">
         <img class="carousel-item__img" src="../images/Portada_1.jpg">
@@ -59,6 +60,45 @@
         </div>
       </div>
     </div>
-  </section>  
+  </section>
+  <div class="Pop-Section">
+    <div class="Pop-Section__title">
+      <h1> POP </h1>
+    </div>
+    <div class="Pop-Section__content">
+      <div class="Pop-Section__row1">
+        <p> Disfruta de los artistas Pop más populares de la actualidad xd</p>
+      </div>
+      <div class="Pop-Section__row2">
+        <p> Disfruta de los artistas Pop más populares de la actualidad</p>
+      </div>
+    </div>
+  </div><br>
+  <div class="Rock-Section">
+    <div class="Rock-Section__title">
+      <h1> ROCK </h1>
+    </div>
+    <div class="Rock-Section__content">
+      <div class="Rock-Section__row1">
+        <p> Disfruta de los artistas Pop más populares de la actualidad xd</p>
+      </div>
+      <div class="Rock-Section__row2">
+        <p> Ve en vivo a los artistas Rock más icónicos</p>
+      </div>
+    </div>
+  </div>
+  <div class="Reggaeton-Section">
+    <div class="Reggaeton-Section__title">
+      <h1> REGGAETON </h1>
+    </div>
+    <div class="Reggaeton-Section__content">
+      <div class="Reggaeton-Section__row1">
+        <p> Disfruta de los artistas Pop más populares de la actualidad xd</p>
+      </div>
+      <div class="Reggaeton-Section__row2">
+        <p> El mejor Reggaeton lo vives con nosotros!</p>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
