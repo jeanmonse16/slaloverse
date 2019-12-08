8aad53bf501c9d32d634919114bc4c23621fca8f
jeanpier <jeanpier.dev@gmail.com>
2019-12-08 10:36:55 -0400

    tickets page

---
+++ view/tickets.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv=”Expires” content=”0″>
+    <meta http-equiv="Pragma" content="no-cache">
+    <link rel="stylesheet" href="../styles/tickets.css">
+    <title>Obtén tu ticket</title>
+</head>
+<body>
+  <div class="navbar">
+    <div class="navbar__row1">
+      <a><img src="../images/Slaloverse2.png" /><a>
+      <a>Tickets<a>
+      <a>Slalo Awards<a>
+    </div>
+    <div class="navbar__row2">
+      <a href="./login">Sign In<a>
+      <a>Sign Up<a>
+    </div>
+  </div>  
+  <div class="tickets-container">
+    <div class="tickets-container___row">
+    <div className="Badge">
+       <div className="Badge__header">
+           <img width="200"src="../images/Slaloverse2.png"></img>
+       </div>
+
+       <div className="Badge__section-name">
+           <h1>Jeanpier <br></br>Monserrate</h1>
+       </div>
+       
+       <div className="Badge__section-info ">
+         <h5>  Monserrate@gmail.com </h5>
+         <p> id </p> 
+       </div>
+       
+       <div className="Badge__footer">
+          #slaloverse
+       </div>
+       
+       </div>
+    </div>
+    <div class="tickets-container___row">
+        hola
+    </div>
+  </div>
+</body>
+</html>
