4be8bc27b380f3784653133a02ef0e5d0ce51bf9
jeanpier <jeanpier.dev@gmail.com>
2019-12-09 00:43:39 -0400

    slalo awards

--- view/slaloawards.php
+++ view/slaloawards.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Pragma" content="no-cache">
     <link rel="stylesheet" href="../styles/slaloawards.css">
     <title>Document</title>
 </head>
@@ -19,8 +20,99 @@
       <a>Sign Up<a>
     </div>
   </div>
-  <div>
-      <img src="../images/Portada_3.jpg" alt="">
+  <div class="AwardSection">
+  </div>
+  <div class="ArtistYear-Section">
+    <div class="ArtistYear__title">
+      <h1>Nominees for Artist of the Year Award</h1>
+    </div>
+    <div class="ArtistYear-nominee">
+      <div class="ArtistYear-img">
+       <img src="../images/BillieEilish_2.png" alt="">
+      </div>
+      <h2>Billie Eilish</h2>
+    </div>
+    <div class="ArtistYear-nominee">
+      <div class="ArtistYear-img">
+       <img src="../images/TOP_2.png" alt="">
+      </div>
+      <h2>twenty one pilots</h2>
+    </div>
+    <div class="ArtistYear-nominee">
+      <div class="ArtistYear-img">
+       <img src="../images/PostMalone_2.png" alt="">
+      </div>
+      <h2>Post Malone</h2>
+    </div>
+  </div>
+  <div class="ArtistPop-Section">
+    <div class="ArtistPop__title">
+      <h1>Nominees for best Pop Artist  Award</h1>
+    </div>
+    <div class="ArtistPop-nominee">
+      <div class="ArtistPop-img">
+       <img src="../images/BillieEilish_2.png" alt="">
+      </div>
+      <h2>Billie Eilish</h2>
+    </div>
+    <div class="ArtistPop-nominee">
+      <div class="ArtistPop-img">
+       <img src="../images/TOP_2.png" alt="">
+      </div>
+      <h2>twenty one pilots</h2>
+    </div>
+    <div class="ArtistPop-nominee">
+      <div class="ArtistPop-img">
+       <img src="../images/Coldplay_2.png" alt="">
+      </div>
+      <h2>Coldplay</h2>
+    </div>
+  </div>
+  <div class="ArtistYear-Section">
+    <div class="ArtistYear__title">
+      <h1>Nominees for best Rock Artist Award</h1>
+    </div>
+    <div class="ArtistYear-nominee">
+      <div class="ArtistYear-img">
+       <img src="../images/Slipknot_2.png" alt="">
+      </div>
+      <h2>Slipknot</h2>
+    </div>
+    <div class="ArtistYear-nominee">
+      <div class="ArtistYear-img">
+       <img src="../images/LosMesoneros_2.png" alt="">
+      </div>
+      <h2>Los Mesoneros</h2>
+    </div>
+    <div class="ArtistYear-nominee">
+      <div class="ArtistYear-img">
+       <img src="../images/GunsnRoses.png" alt="">
+      </div>
+      <h2> Guns n Roses</h2>
+    </div>
+  </div>
+  <div class="ArtistPop-Section">
+    <div class="ArtistPop__title">
+      <h1>Nominees for best Reggaeton Artist  Award</h1>
+    </div>
+    <div class="ArtistPop-nominee">
+      <div class="ArtistPop-img">
+       <img src="../images/DaddyYankee_2.png" alt="">
+      </div>
+      <h2>Daddy Yankee</h2>
+    </div>
+    <div class="ArtistPop-nominee">
+      <div class="ArtistPop-img">
+       <img src="../images/DannyOcean_2.png" alt="">
+      </div>
+      <h2>Danny Ocean</h2>
+    </div>
+    <div class="ArtistPop-nominee">
+      <div class="ArtistPop-img">
+       <img src="../images/JBalvin_2.png" alt="">
+      </div>
+      <h2>J Balvin</h2>
+    </div>
   </div>
 </body>
 </html>
