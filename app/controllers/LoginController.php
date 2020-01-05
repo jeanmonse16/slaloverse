b44fd15c8f8d8bc8c4bc83b436ffab6087d6d91c
Jeanpier Monserrate <monserrateluisje@gmail.com>
2020-01-05 00:47:48 -0400

    Update LoginController.php

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -12,17 +12,6 @@ require_once '../../vendor/autoload.php';
 
 $capsule = new Capsule;
 
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'slaloverse',
-    'username'  => 'root',
-    'password'  => 'twentyonepilots14.',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_general_ci',
-    'prefix'    => '',
-]);
-
 $capsule->setAsGlobal();
 
 $capsule->bootEloquent();
